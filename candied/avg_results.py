7b7f02ba48fbaa0f650a4fb2eab675d8eb9aa4f5
BartoszBiernacki <bartosz.biernacki@int.pl>
2022-01-22 13:16:29 +0100

    Average model DataCollector handling

---
+++ candied/avg_results.py
@@ -0,0 +1,159 @@
+"""Averages model level DataCollector results and saves them to files."""
+import itertools
+import pandas as pd
+import numpy as np
+from pathlib import Path
+
+from collections import namedtuple
+
+
+def group_tuples_by_start(list_of_tuples, start_length):
+    """
+    Returns dict in which:
+        Keys are unique sub-tuples of length 'start_length' created from all
+        tuples in 'list_of_tuples'.
+        Item is a sub-list of tuples contain tuples from 'list_of_tuples'
+        that starts with a key sub-tuple.
+        
+    Example:
+        list_of_tuples = [(1, 2, 5, 6), (1, 2, 4, 7), (1, 3, 9, 4), (2, 3, 5)]
+        start_length = 2
+        result = {
+        (1, 2): [(1, 2, 5, 6), (1, 2, 4, 7)],
+        (1, 3): [(1, 3, 9, 4)],
+        (2, 3): [(2, 3, 5)]
+        }
+    
+    
+    Developed to group model DataCollector results such each group contains
+    unique model calling parameters. To achieve this pass following arguments:
+    
+    list_of_tuples: designed to be list of tuples returned as model data
+        collector keys. Such tuple has following structure:
+        (variable_params_values, fixed_params_values, num_of_iteration),
+        order of items in 'variable_params_values' and 'fixed_params_values'
+        is the same as it was passed to BatchRunner.
+        
+    start_length: to properly group DataCollector results this value should
+        equal to number of 'variable_parameters'.
+    """
+    result = {}
+    tuples_starts = [[item for i, item in enumerate(tup) if i < start_length] for tup in list_of_tuples]
+    tuples_starts.sort()
+    unique_tuples_starts = list(tuples_starts for tuples_starts, _ in itertools.groupby(tuples_starts))
+
+    for unique_tuple_start in unique_tuples_starts:
+        tuples_grouped_by_start = []
+        for tup in list_of_tuples:
+            tuple_start = tup[:start_length]
+            if list(tuple_start) == unique_tuple_start:
+                tuples_grouped_by_start.append(tup)
+        result[tuple(unique_tuple_start)] = tuples_grouped_by_start
+    
+    return result
+
+
+def avg_model_results(model_collector_result,
+                      variable_params,
+                      ):
+    """
+    Returns dict in which:
+        keys are NamedTuples of variable_params
+        values are dataframes averaged over all iterations
+    
+    Function takes:
+        model collector result
+        dict 'variable_parameters' passed to BatchRunner
+    """
+    num_of_variable_model_params = len(variable_params)
+    num_of_variable_model_params = len(model_collector_result)-2
+    list_of_tuples = list(model_collector_result.keys())
+    tuples_grouped = group_tuples_by_start(list_of_tuples=list_of_tuples,
+                                           start_length=num_of_variable_model_params)
+    
+    result = {}
+    # Key is a tuple by which other tuples were grouped, e.g. key=(5, 2, 2)
+    for key in tuples_grouped.keys():
+        lis = []
+        # items are full tuples, e.g. item=(5, 2, 2, ..., 0)
+        for item in tuples_grouped[key]:
+            # list of results dataframes matching key_tuple=(5, 2, 2)
+            lis.append(model_collector_result[item])
+        array_with_all_iterations_results_for_specific_parameters =\
+            np.array(lis)
+        
+        average_array = np.mean(
+            array_with_all_iterations_results_for_specific_parameters, axis=0)
+        df = pd.DataFrame(data=average_array)
+        df.columns = model_collector_result[list_of_tuples[0]].columns
+        
+        # make result keys looks like: ,KeyTuple(n_candies=50, n_creatures=40)'
+        KeyTuple = namedtuple('KeyTuple', variable_params)
+        new_key = {}
+        for i, key_name in enumerate(variable_params):
+            new_key[key_name] = key[i]
+        result[KeyTuple(**new_key)] = df
+    
+    return result
+
+
+def save_avg_results(avg_results,
+                     fixed_params,
+                     variable_params,
+                     save_dir,
+                     runs,
+                     base_params=None):
+    """
+    Saves averaged model level DataCollector results (dataframes) to csv files.
+    
+    base_params are responsible for folder name
+    variable_params are responsible for file names.
+    
+    avg_results: data to be saved.
+    fixed_params: fixed_params passed to BatchRunner.
+    variable_params: variable_params passed to BatchRunner.
+    save_dir: directory where results will be saved.
+    runs: number of simulations performed for one set of parameters. It's
+        useful to have that info in filename.
+    base_params: sub-list of model  fixed parameters names that will be
+    included in name of folder that stores the results.
+    
+    """
+    if base_params:
+        dir_name = '/' + f'Runs={runs}___'
+        for param, val in {**fixed_params, **variable_params}.items():
+            if param in base_params:
+                try:
+                    dir_name += param.capitalize() + '=' + str(
+                        round(val, 3)) + '___'
+                except TypeError:
+                    tuple_val = tuple([round(x, 3) for x in val])
+                    dir_name += param.capitalize() + '=' + str(
+                        tuple_val) + '___'
+
+        dir_name = dir_name[: -3]
+        save_dir += dir_name + '/'
+        save_dir += 'raw data/'
+
+    Path(save_dir).mkdir(parents=True, exist_ok=True)
+
+    try:
+        file_id = len([path for path in Path("./" + save_dir).iterdir() if
+                       path.is_file()])
+    except FileNotFoundError:
+        file_id = 0
+
+    # Saving dataframes
+    for tuple_key, df in avg_results.items():
+        fname = f'Id={str(file_id).zfill(4)}'
+        for param, val in zip(variable_params, tuple_key):
+            try:
+                fname += '___' + param.capitalize() + '=' + str(round(val, 3))
+            except TypeError:
+                tuple_val = tuple([round(x, 3) for x in val])
+                fname += '___' + param.capitalize() + '=' + str(tuple_val)
+
+        df.to_csv(path_or_buf=save_dir + fname + '.csv', index=False)
+        file_id += 1
+
+        
--- run.py
+++ run.py
@@ -1,20 +1,87 @@
 """Run the visualization server."""
 import cProfile
 import pstats
-import matplotlib.pyplot as plt
 
 from candied.server import server
 from mesa.batchrunner import BatchRunner
 from mesa.batchrunner import BatchRunnerMP
 
 from candied.model import Evolution
+from candied.avg_results import avg_model_results
+from candied.avg_results import save_avg_results
 
 
-def run_in_background():
+def run_in_background(fixed_parameters: dict,
+                      variable_parameters: dict,
+                      iterations=3,
+                      multithreading=True,
+                      print_model_data=False,
+                      print_model_avg_data=False,
+                      print_agent_data=False,
+                      ):
+
+    if multithreading:
+        batch_run = BatchRunnerMP(
+            model_cls=Evolution,
+            variable_parameters=variable_parameters,
+            fixed_parameters=fixed_parameters,
+            iterations=iterations,
+            max_steps=fixed_parameters['max_days'],
+        )
+    else:
+        batch_run = BatchRunner(
+            model_cls=Evolution,
+            variable_parameters=variable_parameters,
+            fixed_parameters=fixed_parameters,
+            iterations=iterations,
+            max_steps=fixed_parameters['max_days'],
+        )
+        
+    batch_run.run_all()
+    model_data = batch_run.get_collector_model()  # For each model reporters
+    agent_data = batch_run.get_collector_agents()  # For each agent reporter
+
+    avg_model_data = avg_model_results(model_collector_result=model_data,
+                                       variable_params=variable_parameters)
+    
+    save_avg_results(avg_results=avg_model_data,
+                     fixed_params=fixed_parameters,
+                     variable_params=variable_parameters,
+                     save_dir='results/',
+                     runs=iterations,
+                     base_params=['speed',
+                                  'mut_rate',
+                                  'energy',
+                                  'view_range',
+                                  'max_steps_per_day',
+                                  ]
+                     )
+
+    if print_model_data:
+        print('MODEL DATACOLLECTOR RESULTS BELOW -------------------')
+        for key, df in model_data.items():
+            print(key)
+            print(df.to_markdown())
+        
+    if print_model_avg_data:
+        print('AVERAGE MODEL DATACOLLECTOR RESULTS BELOW -------------------')
+        for key, df in avg_model_data.items():
+            print(key)
+            print(df.to_markdown())
+
+    if print_agent_data:
+        print('AGENT DATACOLLECTOR RESULTS BELOW -------------------')
+        for key, df in agent_data.items():
+            print(key)
+            df = df.loc[df["Agent type"] == 'Creature']
+            print(df.to_markdown())
+
+
+if __name__ == '__main__':
+    # Set up model parameters ---------------
     fixed_params = {
         "height": 200,
         "width": 200,
-        "n_creatures": 40,
         "max_days": 100,
         "energy": 1500,
         "mut_rate": 3,
@@ -22,45 +89,32 @@ def run_in_background():
         "view_range": 8,
         "max_steps_per_day": 100
     }
-
     variable_params = {
-        "n_candies": [100]
+        "n_candies": [50, 60, 100],
+        "n_creatures": [40, 50, 80],
     }
+    multithreading = False
+
+    print_model_data = False
+    print_model_avg_data = True
+    print_agent_data = False
     
-    batch_run = BatchRunner(
-        model_cls=Evolution,
-        variable_parameters=variable_params,
-        fixed_parameters=fixed_params,
-        iterations=1,
-        max_steps=fixed_params['max_days'],
-    )
+    run_in_browser = False
     
-    batch_run.run_all()
-    model_data = batch_run.get_collector_model()  # For each model reporters
-    agent_data = batch_run.get_collector_agents()  # For each agent reporter
-    
-    # for key, df in model_data.items():
-    #     print(key)
-    #     print(df.to_markdown())
-    #     df['Speed'].plot()
-    #
-    #     plt.show()
-    #
-    # for key, df in agent_data.items():
-    #     # print(key)
-    #     df = df.loc[df["Agent type"] == 'Creature']
-    #     print(df.to_markdown())
-
-
-run_in_browser = False
-
-
-if __name__ == '__main__':
+    # Run simulations ----------------------
     if run_in_browser:
         server.launch()
     else:
         with cProfile.Profile() as pr:
-            run_in_background()
+            run_in_background(
+                fixed_parameters=fixed_params,
+                variable_parameters=variable_params,
+                iterations=5,
+                multithreading=multithreading,
+                print_model_data=print_model_data,
+                print_model_avg_data=print_model_avg_data,
+                print_agent_data=print_agent_data,
+            )
         stats = pstats.Stats(pr)
         stats.sort_stats(pstats.SortKey.TIME)
         stats.print_stats(5)
